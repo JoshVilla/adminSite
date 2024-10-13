792659f6159c523c84a1c0dd809f4df194bb1c8b
JoshVilla <villaearljoshua@gmail.com>
2024-10-13 20:00:17 +0800

    optimized top story form

--- src/pages/pageManagement/topStories/index.tsx
+++ src/pages/pageManagement/topStories/index.tsx
@@ -1,24 +1,64 @@
 import TitlePage from "@/components/titlePage/titlePage";
-import React from "react";
+import React, { useEffect } from "react";
 import { CloseOutlined } from "@ant-design/icons";
 import { Button, Card, Form, Input, Space, Typography, Upload } from "antd";
 import TextArea from "antd/es/input/TextArea";
 
-type Props = {};
-
-const TopStories = (props: Props) => {
+const TopStories = () => {
   const [form] = Form.useForm();
+
+  useEffect(() => {
+    console.log("Form Values Updated:", form.getFieldsValue());
+  }, [form]);
+
+  const renderFormItem = (subField: any) => {
+    const type = form.getFieldValue(["items", subField.name, "type"]);
+
+    if (type === "paragraph") {
+      return (
+        <Form.Item
+          name={[subField.name, "paragraph"]}
+          rules={[{ required: true, message: "Please enter a paragraph!" }]}
+        >
+          <TextArea placeholder="Insert Paragraph" />
+        </Form.Item>
+      );
+    }
+
+    if (type === "image") {
+      return (
+        <Form.Item
+          name={[subField.name, "image"]}
+          valuePropName="fileList"
+          getValueFromEvent={(e) => (Array.isArray(e) ? e : e?.fileList)}
+        >
+          <Upload
+            listType="picture-card"
+            maxCount={1}
+            beforeUpload={() => false} // disable auto-upload
+          >
+            <div>+ Upload</div>
+          </Upload>
+        </Form.Item>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <div>
       <TitlePage title="Top Stories" />
       <Form
         form={form}
-        name="dynamic_form_complex"
+        name="dynamic_form"
         style={{ maxWidth: 600 }}
         autoComplete="off"
-        initialValues={{ items: [{}] }}
+        initialValues={{ items: [] }}
       >
+        <Form.Item label="Title" name="title">
+          <Input placeholder="Enter a title" />
+        </Form.Item>
         <Form.List name="items">
           {(fields, { add, remove }) => (
             <div
@@ -27,106 +67,33 @@ const TopStories = (props: Props) => {
               {fields.map((field) => (
                 <Card
                   size="small"
-                  title={`Add Content`}
+                  title={`Item ${field.key + 1}`}
                   key={field.key}
                   extra={<CloseOutlined onClick={() => remove(field.name)} />}
                 >
-                  <Form.Item label="Content">
-                    <Form.List name={[field.name, "content"]}>
-                      {(subFields, subOpt) => (
-                        <div
-                          style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            rowGap: 16,
-                          }}
-                        >
-                          {subFields.map((subField) => {
-                            // Get the type value directly from the form or use the default
-                            const currentType =
-                              form.getFieldValue([
-                                field.name,
-                                "content",
-                                subField.name,
-                                "type",
-                              ]) || "paragraph";
-
-                            return (
-                              <Space key={subField.key}>
-                                <Form.Item
-                                  name={[subField.name, "type"]}
-                                  hidden
-                                  initialValue={currentType} // Initialize type correctly
-                                >
-                                  <Input />
-                                </Form.Item>
-
-                                {currentType === "paragraph" ? (
-                                  <Form.Item
-                                    style={{ width: "450px" }}
-                                    name={[subField.name, "paragraph"]}
-                                    rules={[
-                                      {
-                                        required: true,
-                                        message: "Please enter a paragraph!",
-                                      },
-                                    ]}
-                                  >
-                                    <TextArea
-                                      showCount
-                                      maxLength={100}
-                                      placeholder="Insert Paragraph"
-                                    />
-                                  </Form.Item>
-                                ) : currentType === "image" ? (
-                                  <Form.Item
-                                    name={[subField.name, "image"]}
-                                    valuePropName="fileList"
-                                    getValueFromEvent={(e) =>
-                                      Array.isArray(e) ? e : e?.fileList
-                                    }
-                                  >
-                                    <Upload
-                                      listType="picture-card"
-                                      maxCount={1}
-                                      beforeUpload={() => false} // disable auto-upload
-                                    >
-                                      <div>+ Upload</div>
-                                    </Upload>
-                                  </Form.Item>
-                                ) : null}
-
-                                <CloseOutlined
-                                  onClick={() => subOpt.remove(subField.name)}
-                                />
-                              </Space>
-                            );
-                          })}
-
-                          <Space>
-                            <Button
-                              type="dashed"
-                              onClick={() => {
-                                subOpt.add({ type: "paragraph" }); // Adding paragraph
-                              }}
-                            >
-                              + Add Paragraph
-                            </Button>
-                            <Button
-                              type="dashed"
-                              onClick={() => {
-                                subOpt.add({ type: "image" }); // Adding image
-                              }}
-                            >
-                              + Add Image
-                            </Button>
-                          </Space>
-                        </div>
-                      )}
-                    </Form.List>
-                  </Form.Item>
+                  {renderFormItem(field)}
                 </Card>
               ))}
+
+              <Space>
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add({ type: "paragraph" }); // Add paragraph
+                  }}
+                >
+                  + Add Paragraph
+                </Button>
+
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add({ type: "image" }); // Add image
+                  }}
+                >
+                  + Add Image
+                </Button>
+              </Space>
             </div>
           )}
         </Form.List>
